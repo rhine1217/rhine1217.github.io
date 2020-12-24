1dc3577b82c66b0a41ffb02efd1a42d10bab1e68
rhine1217 <73804887+rhine1217@users.noreply.github.com>
2020-12-23 22:13:01 -0800

    script cleanup and pokemon imgs

--- pokemon-battle-game/script.js
+++ pokemon-battle-game/script.js
@@ -1,10 +1,12 @@
 
+/* Game Configurations */
+
 const gameSettings = {
     playerNum: 2, // default 2 players 
-    pokeDex: 151,
-    pokeNum: 3,
-    playerNames: ['rhine', 'chris'],
-    typeMatchUp: {},
+    pokeDex: 151, // default 1st gen Pokemons only
+    pokeNum: 3, // each player gets 3 Pokemons
+    playerNames: ['rhine', 'chris'], // default names - to update later
+    typeMatchUp: {}, // table matrix to determine attack effectiveness
 }
 
 const defaultTypeObj = {
@@ -28,63 +30,7 @@ const defaultTypeObj = {
     "ground":1
 }
 
-
-let getTypeMatchById = [];
-
-const initTypeMatch = function() {
-    for (let i = 1; i < 19; i++) {
-    
-    getTypeMatchById.push(
-            $.ajax({
-                url: `https://pokeapi.co/api/v2/type/${i}`
-
-            }).then(
-                (data) => {
-
-                    gameSettings.typeMatchUp[data.name] = { ...defaultTypeObj}
-
-                    data['damage_relations']['double_damage_to'].forEach(type => {
-                        gameSettings.typeMatchUp[data.name][type.name] = 2
-                    })
-
-                    data['damage_relations']['half_damage_to'].forEach(type => {
-                        gameSettings.typeMatchUp[data.name][type.name] = 0.5
-                    })
-
-                    data['damage_relations']['no_damage_to'].forEach(type => {
-                        gameSettings.typeMatchUp[data.name][type.name] = 0
-                    })
-
-                },
-
-                (error) => {
-                    console.log('bad request: ',error);
-                })
-    )}
-}
-
-initTypeMatch()
-
-$.when.apply($, getTypeMatchById).done(function() {
-    
-    initPokemon()
-
-    $.when.apply($, getPokemonById).done(function() {
-
-        initPlayers()
-
-    })
-    
-})
-
-const getAllTypes = function (pokeDetails) {
-    let types = [];
-    pokeDetails.types.forEach (type => {
-        types.push(type.type.name)
-    })
-    return types
-}
-
+/* Classes: Pokemon, Player */
 
 class Pokemon {
     constructor(id, pokeDetails) {
@@ -120,36 +66,68 @@ class Player {
     }
 }
 
-// class Move {
-//     constructor(id) {
-//         this.id = id,
-//         this.name = 'move',
-//         this.type = '',
-//         this.damage = '',
-//     }
-// }
+/* Define helper variables for initializing Pokemons and Players */
+
+let getTypeMatchById = []; // list of all the API Calls to get the type match up table 
+let getPokemonById = []; // list of all the API Calls to get Pokemon details (* 6 times)
+let getMoveByUrl; // API call to get Move details (1 per Pokemon)
+let newPokemonList = []; // store all the generated Pokemons
+let players = []; // store the Players
+
+/* Helper Functions */
+
+const getAllTypes = function (pokeDetails) {
+    let types = [];
+    pokeDetails.types.forEach (type => {
+        types.push(type.type.name)
+    })
+    return types
+}
 
 
+/* Main Functions for Initializing */
 
+// Initialize Type Matchup Table (in gameSettings)
 
-let getPokemonById = []; // list of all the API Calls
-let getMoveByUrl;
-let newPokemonList = [];
-let players = [];
+const initTypeMatch = function() {
+    for (let i = 1; i < 19; i++) {
+    
+    getTypeMatchById.push(
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/type/${i}`
+        }).then(
+            (data) => {
+                gameSettings.typeMatchUp[data.name] = { ...defaultTypeObj}
+
+                data['damage_relations']['double_damage_to'].forEach(type => {
+                    gameSettings.typeMatchUp[data.name][type.name] = 2
+                })
+
+                data['damage_relations']['half_damage_to'].forEach(type => {
+                    gameSettings.typeMatchUp[data.name][type.name] = 0.5
+                })
+
+                data['damage_relations']['no_damage_to'].forEach(type => {
+                    gameSettings.typeMatchUp[data.name][type.name] = 0
+                })
+            },
+            (error) => {
+                console.log('bad request: ',error);
+            })
+    )}
+}
+
+// Initialize Pokemons (* 6, 2 for each player)
 
 const initPokemon = function () {
 
     for (let i = 0; i < gameSettings.pokeNum * gameSettings.playerNum; i++) {
 
-    // randomly generate an ID 
-
-        const randomID = Math.floor(Math.random() * gameSettings.pokeDex) + 1 
+        const randomID = Math.floor(Math.random() * gameSettings.pokeDex) + 1 // randomly generate an ID 
 
         getPokemonById.push( 
-        
             $.ajax({
                 url: `https://pokeapi.co/api/v2/pokemon/${randomID}`
-
             }).then(
                 (data) => {
                     
@@ -161,21 +139,19 @@ const initPokemon = function () {
                         stats: data.stats,
                         pics: data.sprites,
                     }
-
-                    let moveUrl = data.moves[Math.floor(Math.random() * data.moves.length) + 1].move.url
-
                     const moveDetails = {
                         power: null,
                         type: null,
                         name: null,
                     };
 
+                    let moveUrl = data.moves[Math.floor(Math.random() * data.moves.length) + 1].move.url
+
                     initMove(moveUrl, moveDetails)
 
                     const getNewMove = function() {
                         if (moveDetails.power !== null) {
                             pokeDetails.move = moveDetails 
-                            // console.log(pokeDetails) // tester
                             newPokemonList.push(new Pokemon(pokeID, pokeDetails))
                         } else {
                             moveUrl = data.moves[Math.floor(Math.random() * data.moves.length) + 1].move.url
@@ -186,7 +162,6 @@ const initPokemon = function () {
 
                     $.when(getMoveByUrl).done(getNewMove);
                 },
-
                 (error) => {
                     console.log('bad request: ',error);
                 }
@@ -195,32 +170,45 @@ const initPokemon = function () {
     }
 }
 
+// Initialize Players 
 
 const initPlayers = function() {
     
     players = [new Player(gameSettings.playerNames[0]), new Player(gameSettings.playerNames[1])]
-    
     players[0].pokemon = newPokemonList.slice(0,3)
     players[1].pokemon = newPokemonList.slice(3,6)
         
 }
 
-
+// Initialize a random move for each Pokemon (used in initPokemon function)
 
 const initMove = function(moveUrl, moveDetails) {
-
     getMoveByUrl = 
-    
         $.ajax({ url: moveUrl }).then(
-        
             (data) => {
                 moveDetails.power = data.power
                 moveDetails.type = data.type.name
                 moveDetails.name = data.name
             },
-
             (error) => {
                 console.log('bad request: ',error);
             }
         )
 }
+
+
+/* Main Script */
+
+initTypeMatch() // fill the gameSettings with type matchup table
+
+$.when.apply($, getTypeMatchById).done(function() { // after the type matchup table is filled
+    
+    initPokemon() // initalize Pokemons (with all details including move assignment)
+
+    $.when.apply($, getPokemonById).done(function() { // after Pokemon initialization
+
+        initPlayers() // initialize Players
+
+    })
+    
+})
